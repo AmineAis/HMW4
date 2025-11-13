4ebcec47d96cb9a3d8817c4320bbf0d83c18930b
AmineAis <ma43963n@pace.edu>
2025-11-12 23:03:21 -0500

    ScannerDemo.java

--- ScannerDemo (1).java
+++ ScannerDemo (1).java
@@ -4,16 +4,17 @@
  * 
  */
 public class ScannerDemo {
-
+	
+	
 	// This needs to be changed !
-	private static String file1 = "C:\\Users\\cscharff\\eclipse-workspace\\ScannerSolution\\src\\prog1.jay";
+	private static String file1 = "KAY.text";
 	private static int counter = 1;
 
 	public static void main(String args[]) {
 		TokenStream ts = new TokenStream(file1);
 		System.out.println(file1);
 		while (!ts.isEndofFile()) {
-			// TO BE COMPLETED
+			Token tk = ts.nextToken();
 			System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
 		}
 	}
